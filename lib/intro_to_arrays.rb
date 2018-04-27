9839fe09e4a64a742295f0551da2ded8d8400f58
Roniece A Ricardo <roniecericardo@gmail.com>
2018-04-27 20:38:12 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -9,3 +9,19 @@ end
 def first_element(array)
   array.first
 end
+
+def my_third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array.last
+end
+
+def first_element_with_array_methods(array)
+  first_element(array)
+end
+
+def last_element_with_array_methods(array)
+  last_element(array)
+end
